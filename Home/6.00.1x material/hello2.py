45b13f0170a917ff69b833cd1f142897e5192a30
RobinTian0916 <RobinTian0916@gmail.com>
2022-05-22 15:11:27 +0800

    Chapter11

---
+++ Home/6.00.1x material/hello2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 22:14:35 2021
+
+print('hello world')
+
---
+++ Home/6.00.1x material/untitled0.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 22:14:35 2021
+
+@author: tianz
+"""print('hello world')
+
